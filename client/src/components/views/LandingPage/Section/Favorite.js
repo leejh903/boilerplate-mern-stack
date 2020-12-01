c2d11f09396b7d5a8e72a9a02297ff018e78030d
Brad Lee <brad903@naver.com>
2020-12-01 20:38:49 +0900

    implemented favorite page

--- client/src/components/views/LandingPage/Section/Favorite.js
+++ client/src/components/views/LandingPage/Section/Favorite.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Axios from "axios";
+import {Button} from 'antd';
 
 function Favorite(props) {
 
@@ -12,12 +13,15 @@ function Favorite(props) {
     const [FavoriteNumber, setFavoriteNumber] = useState(0);
     const [Favorited, setFavorited] = useState(false);
 
-    useEffect(() => {
+    let variables = {
+        userFrom: userFrom,
+        movieId: movieId,
+        movieTitle: movieTitle,
+        moviePost: moviePost,
+        movieRunTime: movieRunTime
+    }
 
-        let variables = {
-            userFrom,
-            movieId
-        }
+    useEffect(() => {
 
         Axios.post('/api/favorite/favoriteNumber', variables)
             .then(response => {
@@ -44,9 +48,36 @@ function Favorite(props) {
 
     }, [])
 
+
+    const onClickFavorite = () => {
+
+        if (Favorited) {
+            Axios.post('/api/favorite/removeFromFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber - 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 지우는 걸 실패했습니다.')
+                    }
+                })
+        } else {
+            Axios.post('/api/favorite/addToFavorite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteNumber(FavoriteNumber + 1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 추가하는 걸 실패했습니다.')
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <button>{Favorited? " Not Favorite" : "Add to Favorite "} {FavoriteNumber} </button>
+            <Button
+                onClick={onClickFavorite}>{Favorited ? " Not Favorite" : "Add to Favorite "} {FavoriteNumber} </Button>
         </div>
     );
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -4,7 +4,6 @@ const {Favorite} = require('../models/Favorite')
 
 router.post('/favoriteNumber', (req, res) => {
 
-
     Favorite.find({"movieId": req.body.movieId})
         .exec((err, info) => {
             if (err) return res.status(400).send(err)
@@ -16,7 +15,6 @@ router.post('/favoriteNumber', (req, res) => {
 
 router.post('/favorited', (req, res) => {
 
-
     Favorite.find({"movieId": req.body.movieId, "userFrom": req.body.userFrom})
         .exec((err, info) => {
             if (err) return res.status(400).send(err)
@@ -27,6 +25,47 @@ router.post('/favorited', (req, res) => {
             }
 
             res.status(200).json({success: true, favorited: result})
+        })
+
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+
+    Favorite.findOneAndDelete({movieId: req.body.movieId, userFrom: req.body.userFrom})
+        .exec((err, doc) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({success: true, doc})
+        })
+
+})
+
+router.post('/addToFavorite', (req, res) => {
+
+    const favorite = new Favorite(req.body)
+
+    favorite.save((err, doc) => {
+        if (err) return res.status(400).send(err)
+        return res.status(200).json({success: true})
+    })
+
+})
+
+router.post('/getFavoriteMovie', (req, res) => {
+
+    Favorite.find({'userFrom': req.body.userFrom})
+        .exec((err, favorites) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({success: true, favorites})
+        })
+
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+
+    Favorite.findOneAndDelete({movieId: req.body.movieId, userFrom: req.body.userFrom})
+        .exec((err, result) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({success: true})
         })
 
 })
